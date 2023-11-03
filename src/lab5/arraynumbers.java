3690811eaca5dadb3e75efc4852ff6728f506378
Kaushal <jasanikaushal@yahoo.com>
2023-11-03 14:26:39 +0100

    lab5

---
+++ src/lab5/arraynumbers.java
@@ -0,0 +1,17 @@
+package lab5;
+
+public class arraynumbers {
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+
+
+        for (int i = 0; i < 10; i++) {
+            numbers[i] = 9 - i;
+        }
+
+        for (int i =0; i < 10; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+
+}
