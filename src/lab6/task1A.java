16ac86e0d134a615ebfd6fffb435c589e5b64169
Kaushal <jasanikaushal@yahoo.com>
2023-11-10 13:02:44 +0100

    lab5

--- src/lab6/task1A.java
+++ src/lab6/task1A.java
@@ -1,16 +1,15 @@
 package lab6;
 
+import java.util.Arrays;
+
 public class task1A {
     public static void main(String[] args) {
-        int[] array = new int[10];
+        int[] array = {10,9,8,7,6,5,4,3,2,1};
 
-        for (int i = 0; i < 10; i++) {
-            array[i] = 10 - i;
-        }
-        System.out.print("Before: ");
-        for (int i = 0; i < 10; i++) {
-            System.out.print(array[i] + " ");
-        }
+        int counter = 0;
+        System.out.println("Before:");
+        System.out.println(Arrays.toString(array));
+
         for (int i = 0; i < array.length - 1; i++) {
             if (array[i] > array[i + 1]) {
                 int temp = array[i];
