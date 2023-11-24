c7e0fd0750a6c07978ebc2397c02b25d1502b3cd
Kaushal <jasanikaushal@yahoo.com>
2023-11-24 12:52:20 +0100

    lab5

--- src/lab7/Mammal.java
+++ src/lab7/Mammal.java
@@ -3,6 +3,7 @@ package lab7;
 public class Mammal extends Animal {
 
     public Mammal(String name, int age, double weight) {
+
         super(name, age, weight);
     }
 
--- src/lab7/task2.java
+++ src/lab7/task2.java
@@ -2,12 +2,14 @@ package lab7;
 
 public class task2 {
         public static void main(String[] args) {
-            Animal[] animals = new Animal[3];
+            Animal[] animals = new Animal[6];
 
-            animals[0] = new Dog("Buddy", 3, 12.5, "lebra");
+            animals[0] = new Dog("Black", 3, 12.5, "lebra");
             animals[1] = new Blowfish("Bubbles", 1, 0.5, "gold");
             animals[2] = new Pigeon("Feather", 2, 0.8, "Gray", "Rock Pigeon");
-
+            animals[3] = new Mammal("Mammal", 5, 20.0);
+            animals[4] = new Bird("Peacock", 2, 0.5, "Blue");
+            animals[5] = new Fish("Goldie", 1, 0.2);
 
             for (Animal animal : animals) {
                 System.out.println(animal.toString());
